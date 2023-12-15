b53e90f02fc3aeb0854f014bac31b91bc95c36c6
Mukesh <billamukesh1234@gmail.com>
2023-12-15 19:53:35 +0530

    about section✅

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -2,9 +2,6 @@ const Leaderboard = () => {
   return (
     <div className="leaderboard-section">
       <h1 className="title">Revealing Soon</h1>
-      {/* <div className="lb-content">
-        
-      </div> */}
     </div>
   )
 }
