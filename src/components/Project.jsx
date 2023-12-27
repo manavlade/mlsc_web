76e3a957c953f34b8ccc322ddd31b1bb2f360409
Mukesh <billamukesh1234@gmail.com>
2023-12-27 17:58:56 +0530

    events half completed✅

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,9 @@ const Project = () => {
         <>
             <section className="project_section">
                 <h2 className="title">Projects</h2>
-                <div></div>
+                <div>
+                    
+                </div>
             </section>
         </>
     )
