0859167c3211bdbcf5419af96fd3ca7fd9cc3286
Mukesh <billamukesh1234@gmail.com>
2023-12-29 18:09:22 +0530

    project section completed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,47 @@
-import dummyImg from "../assets/azurechallenge.jpeg"
-import dummyImg2 from "../assets/reactjsworkshop.jpeg"
-import dummyImg3 from "../assets/azurechallenge.jpeg"
-// import { useInView } from "react-intersection-observer";
+import gsap from "gsap";
+import { useEffect } from "react";
+import { ScrollTrigger } from "gsap/all";
+import dummyImg from "../assets/azurechallenge.jpeg";
+import dummyImg2 from "../assets/reactjsworkshop.jpeg";
+import dummyImg3 from "../assets/azurechallenge.jpeg";
+
+const projectDetails = [
+    {
+        title: "Project 1",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto velit odio eligendi repellendus veritatis, ex excepturi quod sint ratione deleniti asperiores iure minus fuga sed, eos facilis esse delectus ipsam!",
+    },
+    {
+        title: "Project 2",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto velit odio eligendi repellendus veritatis, ex excepturi quod sint ratione deleniti asperiores iure minus fuga sed, eos facilis esse delectus ipsam!",
+    },
+    {
+        title: "Project 3",
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto velit odio eligendi repellendus veritatis, ex excepturi quod sint ratione deleniti asperiores iure minus fuga sed, eos facilis esse delectus ipsam!",
+    },
+];
 
 const Project = () => {
+    useEffect(() => {
+        gsap.set(".photo:not(:first-child)", { yPercent: 101 });
 
-    // const { galleryRef, inView } = useInView({
-    //     threshold: 1,
-    // })
+        const animation = gsap.to(".photo:not(:first-child)", {
+            yPercent: 0,
+            duration: 1,
+            stagger: 1,
+        });
+
+        gsap.registerPlugin(ScrollTrigger);
+
+        ScrollTrigger.create({
+            trigger: ".gallery",
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".right",
+            animation: animation,
+            scrub: true,
+            markers: false,
+        });
+    }, []);
 
     return (
         <>
@@ -17,51 +51,31 @@ const Project = () => {
                 <div className="gallery">
                     <div className="left">
                         <div className="detailsWrapper">
-                            <div className="details">
-                                <h3 className="headline">Project 1</h3>
-                                <div className="text">
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis architecto molestias nesciunt labore rerum unde excepturi modi fugit. Illum soluta, ad error quo enim quam nam, eveniet possimus officia, aperiam quia? Eum quos officiis vitae esse deserunt nam veritatis quidem, eligendi accusamus excepturi? Dicta amet, modi soluta assumenda deleniti quia!</p>
+                            {projectDetails.map((project, index) => (
+                                <div className="details" key={index}>
+                                    <h3 className="headline">{project.title}</h3>
+                                    <div className="text">
+                                        <p>{project.text}</p>
+                                        <button className="proj_btn">View<span>↗</span></button>
+                                    </div>
                                 </div>
-                                <button className="proj_btn">View<span>↗</span></button>
-                            </div>
-
-                            <div className="details">
-                                <h3 className="headline">Project 2</h3>
-                                <div className="text">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At delectus dolorem odit inventore nulla est quas similique excepturi cum ab voluptatibus sint asperiores autem, animi atque alias eaque qui rerum. Porro natus distinctio obcaecati aperiam debitis illo voluptatum in dolores veritatis quisquam voluptate quos corporis, perferendis sunt reprehenderit aut rerum?</p>
-                                    <button className="proj_btn">View<span>↗</span></button>
-                                </div>
-                            </div>
-
-                            <div className="details">
-                                <h3 className="headline">Project 3</h3>
-                                <div className="text">
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum minus sint soluta iusto blanditiis explicabo ducimus itaque facere perferendis temporibus cupiditate excepturi quam et qui commodi ea maxime a eius accusantium, rerum corporis doloremque. Est voluptatum dolore, facere quisquam asperiores praesentium officiis quaerat, odit rem quia, deleniti quam voluptas natus.</p>
-                                </div>
-                                <button className="proj_btn">View<span>↗</span></button>
-                            </div>
-
+                            ))}
                         </div>
                     </div>
 
                     <div className="right">
                         <div className="photos">
-                            <div className="photo">
-                                <img src={dummyImg} />
-                            </div>
-                            <div className="photo">
-                                <img src={dummyImg2} />
-                            </div>
-                            <div className="photo">
-                                <img src={dummyImg3} />
-                            </div>
+                            {[dummyImg, dummyImg2, dummyImg3].map((img, index) => (
+                                <div className="photo" key={index}>
+                                    <img src={img} alt={`Project ${index + 1}`} />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
             </section>
         </>
-    )
+    );
 };
 
 export default Project;
-
